d740cbb12e49edb1c468a9fe2069579103d6993a
Christian Rivadeneira <48138971+A-Souvlaki@users.noreply.github.com>
2022-12-04 16:01:51 -0500

    some minor changes

---
+++ app/src/main/java/com/icesi/fiufiu/model/Seller.kt
@@ -0,0 +1,10 @@
+package com.icesi.fiufiu.model
+
+data class Seller(
+    val id: String="",
+    val sellerName: String="",
+    val email: String="",
+    val phone: String="",
+    val role:String="",
+    var marketID:String=""
+)
---
+++ app/src/main/java/com/icesi/fiufiu/model/User.kt
@@ -0,0 +1,10 @@
+package com.icesi.fiufiu.model
+
+data class User (
+    var id: String= "",
+    var name: String ="",
+    var email: String="",
+    var phone: String="",
+    var img: String="",
+    var role: String=""
+        )
